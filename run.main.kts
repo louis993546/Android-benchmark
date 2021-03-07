7abf5c77bf6ed518b37f83d1997dd5a7711eb653
Louis Tsai <louis993546@gmail.com>
2021-03-07 11:36:43 +0100

    Seperate Java 8 & 11

--- run.main.kts
+++ run.main.kts
@@ -12,33 +12,41 @@ shell {
     val nameOfMachine = args[0]
     val waitTime = args[1].toLong()
 
-    val projects = this.file("projects.csv")
-    val lines = projects.readLines().drop(1)
-    lines.forEachIndexed { index, line ->
-        val row = line.split(',').map { it.trim() }.take(3)
-        val gitUrl = row[0]
-        val commit = row[1]
-        val task = row[2]
-        val (owner, projectGit) = gitUrl.split('/').takeLast(2)
-        val project = projectGit.split('.')[0]
+    suspend fun run(file: String, jdkHome: String) {
+        export("JAVA_HOME" to jdkHome)
 
-        val id = "$owner/$project/$task"
-        println("Starting $id")
+        val projects = this.file(file)
+        val lines = projects.readLines().drop(1)
+        lines.forEachIndexed { index, line ->
+            val row = line.split(',').map { it.trim() }.take(3)
+            val gitUrl = row[0]
+            val commit = row[1]
+            val task = row[2]
+            val (owner, projectGit) = gitUrl.split('/').takeLast(2)
+            val project = projectGit.split('.')[0]
 
-        "git clone $gitUrl repos/$id"()
-        cd("repos/$id")
-        "git checkout $commit"()
-        repeat(4) { cd(up) }
+            val id = "$owner/$project/$task"
+            println("Starting $id")
 
-        "gradle-profiler --benchmark --project-dir repos/$id --output-dir data/$nameOfMachine/$id $task"()
-        "rm -rf repos/$id"()
+            "git clone $gitUrl repos/$id"()
+            cd("repos/$id")
+            "git checkout $commit"()
+            repeat(4) { cd(up) }
 
-        println("${index + 1}/${lines.size}: $id is done.")
-        val isLast = (index + 1) == lines.size
-        if (!isLast) {
-            println("Next one will start in about ${waitTime/1000} seconds / ${waitTime/1000/60} minutes.")
-            Thread.sleep(waitTime)
+            "gradle-profiler --benchmark --project-dir repos/$id --output-dir data/$nameOfMachine/$id $task"()
+            "rm -rf repos/$id"()
+
+            println("${index + 1}/${lines.size}: $id is done.")
+            val isLast = (index + 1) == lines.size
+            if (!isLast) {
+                println("Next one will start in about ${waitTime/1000} seconds / ${waitTime/1000/60} minutes.")
+                Thread.sleep(waitTime)
+            }
         }
     }
+
+    run("java11.csv", "/usr/lib/jvm/java-11-openjdk-amd64")
+    run("java8.csv", "/usr/lib/jvm/java-8-openjdk-amd64")
+
     println("It's all done. Please commit and push when everything looks ok")
 }
